5c0eea039f94e133698330847948d65383f1f03e
dustinrue <ruedu@dustinrue.com>
2014-12-11 12:33:11 -0600

    be more careful on delete, only append tags

--- split_video.sh
+++ split_video.sh
@@ -3,5 +3,10 @@
 SOURCE_FILE="$1"
 DEST_DIR=$2
 
+if [ "${DEST_DIR}" == "" ]; then
+  echo "You must give me a destination dir"
+  exit
+fi
+
 rm -rf ${DEST_DIR}/*.png
 ffmpeg -i "${SOURCE_FILE}"  -vf fps=1/2 -f image2 "${DEST_DIR}/frame-%07d.png"
--- tag_video.sh
+++ tag_video.sh
@@ -5,4 +5,4 @@ VID="$1"
 ./split_video.sh "${VID}" source &> /dev/null
 TAGS=`./find_medal.py -i source`
 echo ${TAGS} ${VID}
-tag -s "${TAGS}" "${VID}"
+tag -a "${TAGS}" "${VID}"
