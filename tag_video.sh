538486f09cd66ccd8bc2836bcd4cdc8ec795c77c
dustinrue <ruedu@dustinrue.com>
2014-12-12 08:55:04 -0600

    remove a bad exit

--- tag_video.sh
+++ tag_video.sh
@@ -6,7 +6,6 @@ BNAME=`basename "${VID}"`
 echo ~/tmp/source/${BNAME}
 mkdir -p ~/tmp/source/${BNAME}
 split_video.sh "${VID}" ~/tmp/source/${BNAME}
-exit
 TAGS=`find_medal.py -i ~/tmp/source/${BNAME}`
 echo ${TAGS} ${VID}
 tag -a "${TAGS}" "${VID}"
